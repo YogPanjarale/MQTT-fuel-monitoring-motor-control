f73718760e55661330e0254f7c91cedb8d407f89
Yog <yog.panjarale@gmail.com>
2021-08-26 17:47:20 +0530

    water sensor working !

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,28 @@
 int thermoDO = 25;
 int thermoCS = 26;
 int thermoCLK = 27;
-
+int waterSensor = 14;
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
-  Serial.println("MAX6675 test");
+  pinMode(waterSensor, INPUT);
   // wait for MAX chip to stabilize
   delay(500);
 }
 
-void loop() {
+void loop()
+{
   // basic readout test, just print the current temp
-  
-   Serial.print("C = "); 
-   Serial.println(thermocouple.readCelsius());
-   Serial.print("F = ");
-   Serial.println(thermocouple.readFahrenheit());
- 
-   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
-   delay(1000);
+
+  Serial.print("C = ");
+  Serial.println(thermocouple.readCelsius());
+  Serial.print("F = ");
+  Serial.println(thermocouple.readFahrenheit());
+
+  Serial.printf("Water sensor : %s\n", digitalRead(waterSensor) == HIGH ? "Off" : "On");
+  // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
+  delay(1000);
 }
