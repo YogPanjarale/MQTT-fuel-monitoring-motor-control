2df1a5b60d6b84d16a159430a2454c440655b5ae
Yog <yog.panjarale@gmail.com>
2021-09-13 20:14:51 +0530

    rpm sensor added

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,28 @@
 int thermoDO = 25;
 int thermoCS = 26;
 int thermoCLK = 27;
-int waterSensor = 14;
+int waterSensor = 32;
+int rpmSensor = 14;
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
-
+int delay1()
+{
+  //unsigned int long k;
+  int i, j;
+  unsigned int count = 0;
+  for (i = 0; i < 1000; i++)
+  {
+    for (j = 0; j < 1000; j++)
+    {
+      if (digitalRead(rpmSensor))
+      {
+        count++;
+        while (digitalRead(rpmSensor))
+          ;
+      }
+    }
+  }
+  return count;
+}
 void setup()
 {
   Serial.begin(9600);
@@ -16,17 +35,25 @@ void setup()
   // wait for MAX chip to stabilize
   delay(500);
 }
-
 void loop()
 {
   // basic readout test, just print the current temp
+unsigned int time=0,RPM=0;
+
 
   Serial.print("C = ");
   Serial.println(thermocouple.readCelsius());
   Serial.print("F = ");
   Serial.println(thermocouple.readFahrenheit());
-
+  // Read water sensor value
   Serial.printf("Water sensor : %s\n", digitalRead(waterSensor) == HIGH ? "Off" : "On");
+time=delay1();
+
+RPM=(time*12)/3;
+
+
+  Serial.printf("RPM sensor : %d %d\n", time,RPM);
+  Serial.println(digitalRead(rpmSensor));
   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
   delay(1000);
 }
